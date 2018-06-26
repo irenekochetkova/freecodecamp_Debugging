0db57cefa85f8fcb3e45ee97cd346fe268cf58aa
Irina <irenekochetkova@gmail.com>
2018-06-25 18:36:07 -0700

    added the solutions of the tasks Debugging

---
+++ 09_Catch_Off_By_One_Errors_When_Using_Indexing.js
@@ -0,0 +1,25 @@
+// Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+
+// function countToFive() {
+//   let firstFive = "12345";
+//   let len = firstFive.length;
+//   // Fix the line below
+//   for (let i = 1; i <= len; i++) {
+//   // Do not alter code below this line
+//     console.log(firstFive[i]);
+//   }
+// }
+
+// countToFive();
+// ======================================================
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Fix the line below
+  for (let i = 0; i < len; i++) {
+  // Do not alter code below this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
